f8acb77806da74dee133164af6061467a9477316
Ch Umar Aslam <chumaraslam22@gmail.com>
2024-07-23 10:43:16 +0500

    "fixed deployment error"

--- pages/courses/Assignments.tsx
+++ pages/courses/Assignments.tsx
@@ -1,48 +1,67 @@
-import { paramsType } from "@/data/AssignmentsData";
+import { paramsType } from "@/data/AttendanceData";
 import { AssignmentsData } from "../../data/AssignmentsData";
 import React, { useState } from "react";
 import PopUP from "@/components/PopUP";
 
 export default function Assignments() {
-  const [popupModal, setPopupModal] = useState<paramsType | null>();
-  const showPopupModal = (student: paramsType) => {
-    setPopupModal(student);
-  };
-  const closePopUp = () => {
-    setPopupModal(null);
-  };
-  return (
-    <div>
-      <table className="">
-        <thead className="bg-blue-50">
-          <tr className="grid grid-cols-5 border text-center py-5 gap-32">
-            <th className="">Id</th>
-            <th className="">Name</th>
-            <th className="">Title</th>
-            <th className="">Due Date</th>
-            <th className="">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {AssignmentsData?.map((assignment) => (
-            <tr
-              key={assignment.id}
-              onClick={() => showPopupModal(assignment)}
-              className="grid grid-cols-5 bg-slate-100 border py-7 text-center  gap-40"
-            >
-              <td className="">{assignment.id}</td>
-              <td className="">{assignment.name}</td>
-              <td className="">{assignment.title}</td>
-              <td className="">{assignment.dueDate}</td>
-              <td className="">{assignment.status}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+	const [popupModal, setPopupModal] = useState<
+		any | null
+	>();
+	const showPopupModal = (student: any) => {
+		setPopupModal(student);
+	};
+	const closePopUp = () => {
+		setPopupModal(null);
+	};
+	return (
+		<div>
+			<table className="">
+				<thead className="bg-blue-50">
+					<tr className="grid grid-cols-5 border text-center py-5 gap-32">
+						<th className="">Id</th>
+						<th className="">Name</th>
+						<th className="">Title</th>
+						<th className="">Due Date</th>
+						<th className="">Status</th>
+					</tr>
+				</thead>
+				<tbody>
+					{AssignmentsData?.map((assignment) => (
+						<tr
+							key={assignment.id}
+							onClick={() =>
+								showPopupModal(assignment)
+							}
+							className="grid grid-cols-5 bg-slate-100 border py-7 text-center  gap-40"
+						>
+							<td className="">
+								{assignment.id}
+							</td>
+							<td className="">
+								{assignment.name}
+							</td>
+							<td className="">
+								{assignment.title}
+							</td>
+							<td className="">
+								{assignment.dueDate}
+							</td>
+							<td className="">
+								{assignment.status}
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 
-      <div>
-        {popupModal && <PopUP {...popupModal} closePopUp={closePopUp} />}
-      </div>
-    </div>
-  );
+			<div>
+				{popupModal && (
+					<PopUP
+						{...popupModal}
+						closePopUp={closePopUp}
+					/>
+				)}
+			</div>
+		</div>
+	);
 }
--- pages/courses/Attendance.tsx
+++ pages/courses/Attendance.tsx
@@ -3,41 +3,61 @@ import { AttendanceData } from "../../data/AttendanceData";
 import React, { useState } from "react";
 import { paramsType } from "../../data/AttendanceData";
 export default function Assignments() {
-  const [popupModal, setPopupModal] = useState<paramsType | null>();
-  const showPopupModal = (student: paramsType) => {
-    setPopupModal(student);
-  };
-  const closePopUp = () => {
-    setPopupModal(null);
-  };
-  return (
-    <table className="">
-      <thead className="bg-blue-50">
-        <tr className="grid grid-cols-5 border text-center py-5 gap-32">
-          <th className="">Id</th>
-          <th className="">Name</th>
-          <th className="">Present</th>
-          <th className="">Absent</th>
-          <th className="">Percentage</th>
-        </tr>
-      </thead>
-      <tbody>
-        {AttendanceData?.map((student: paramsType) => (
-          <tr
-            onClick={() => showPopupModal(student)}
-            className="grid grid-cols-5 bg-slate-100 border py-7 text-center cursor-pointer gap-40"
-          >
-            <td className="">{student.id}</td>
-            <td className="">{student.name}</td>
-            <td className="">{student.present}</td>
-            <td className="">{student.absent}</td>
-            <td className="">{student.percentage}%</td>
-          </tr>
-        ))}
-      </tbody>
-      <div>
-        {popupModal && <PopUP {...popupModal} closePopUp={closePopUp} />}
-      </div>
-    </table>
-  );
+	const [popupModal, setPopupModal] =
+		useState<paramsType | null>();
+	const showPopupModal = (student: paramsType) => {
+		setPopupModal(student);
+	};
+	const closePopUp = () => {
+		setPopupModal(null);
+	};
+	return (
+		<table className="">
+			<thead className="bg-blue-50">
+				<tr className="grid grid-cols-5 border text-center py-5 gap-32">
+					<th className="">Id</th>
+					<th className="">Name</th>
+					<th className="">Present</th>
+					<th className="">Absent</th>
+					<th className="">Percentage</th>
+				</tr>
+			</thead>
+			<tbody>
+				{AttendanceData?.map(
+					(student: paramsType) => (
+						<tr
+							onClick={() =>
+								showPopupModal(student)
+							}
+							className="grid grid-cols-5 bg-slate-100 border py-7 text-center cursor-pointer gap-40"
+						>
+							<td className="">
+								{student.id}
+							</td>
+							<td className="">
+								{student.name}
+							</td>
+							<td className="">
+								{student.present}
+							</td>
+							<td className="">
+								{student.absent}
+							</td>
+							<td className="">
+								{student.percentage}%
+							</td>
+						</tr>
+					)
+				)}
+			</tbody>
+			<div>
+				{popupModal && (
+					<PopUP
+						{...popupModal}
+						closePopUp={closePopUp}
+					/>
+				)}
+			</div>
+		</table>
+	);
 }
